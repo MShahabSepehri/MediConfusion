5a776a15fea9414f140f48de1d94b797df41f671
z-fabian <zfabian@usc.edu>
2024-09-06 15:09:07 -0700

    fix blip2, add blip2 prefix, fix instructblip output bug, add blip2 prompt formats to config

--- VLMs/blip2.py
+++ VLMs/blip2.py
@@ -1,6 +1,6 @@
 import torch
 from PIL import Image
-from transformers import AutoProcessor, Blip2ForConditionalGeneration
+from transformers import Blip2Processor, Blip2ForConditionalGeneration
 
 
 def get_device():
@@ -9,18 +9,21 @@ def get_device():
     return "cpu"
 
 def load_model():
-    processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
-    model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16)
+    processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+    model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b")#, torch_dtype=torch.float16)
     model.to(get_device())
     return model, processor
 
 def ask_question(model, question, image_path, processor, num_beams, max_length, top_p, temperature, mode):
+    image = Image.open(image_path).convert("RGB")
+    if mode == 'prefix':
+        return do_prefix_forward(model, question, image, processor)
     if mode == 'greedy':
-        return do_forward(model, processor, image_path, question)
+        return do_forward(model, processor, image, question)
     elif mode in ['mc', 'gpt4']:
         return do_generation(model, 
                             processor, 
-                            image_path, 
+                            image, 
                             question,
                             num_beams=num_beams,
                             top_p=top_p,
@@ -30,42 +33,68 @@ def ask_question(model, question, image_path, processor, num_beams, max_length, 
 @torch.no_grad()
 def do_generation(model, 
                   processor, 
-                  image_path, 
+                  image, 
                   question, 
                   num_beams, 
                   top_p,
                   temperature, 
                   max_new_tokens):
-    image = Image.open(image_path).convert("RGB")
-    # question = "this is a picture of"
-    inputs = processor(image, text=question, return_tensors="pt").to(device="cuda", dtype=torch.float16)
-    outputs = model.generate(
-            **inputs,
-            num_beams=num_beams,
-            max_new_tokens=max_new_tokens,
-            min_length=1,
-            top_p=top_p,
-            temperature=temperature,
-    )
-    generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
-    raise ValueError(generated_text, temperature, top_p, question, inputs.get('input_ids').shape)
+    inputs = processor(images=image, text=question, return_tensors="pt").to(device="cuda", dtype=torch.float16)
+    outputs = model.generate(**inputs, num_beams=num_beams, top_p=top_p, temperature=temperature, max_new_tokens=max_new_tokens)
+    generated_text = processor.decode(outputs[0], skip_special_tokens=True)
+    print(generated_text)
     return generated_text
 
 @torch.no_grad()
-def do_forward(model, processor, image_path, question):
+def do_forward(model, processor, image, question):
     VALID_ANSWERS = ['A', 'B']
-    question = 'The first letter of alphabet: '
     TOKEN_ID_A = processor.tokenizer("A", return_tensors="pt", add_special_tokens=False).get('input_ids')
     TOKEN_ID_B = processor.tokenizer("B", return_tensors="pt", add_special_tokens=False).get('input_ids')
-    image = Image.open(image_path).convert("RGB")
     inputs = processor(images=image, text=question, return_tensors="pt").to(device=get_device(), dtype=torch.float16)
-
-    ss = model.forward(**inputs).logits
-    raise ValueError(ss.shape)
-    ss = ss[0, -1, :]
+    logits = model.forward(**inputs).logits
+    logits = logits[0, -1, :]
     logits = logits.reshape(-1, 1)
     soft_max = torch.nn.Softmax(dim=0)
     probs = soft_max(torch.cat([logits[TOKEN_ID_A], logits[TOKEN_ID_B]][:len(VALID_ANSWERS)]))
     outputs = VALID_ANSWERS[probs.argmax().item()]
-    raise ValueError(probs)
+    return outputs
+
+@torch.no_grad()
+def do_prefix_forward(model, problem, image, processor):
+    PREFIX_PROMPT_TEMPLATE = "Question: {} Answer: {}"
+    #python scripts/answering.py --vlm_name instructblip --mode mc
+    scores = []
+    questions = []
+    qs = problem["question"]
+
+    for option in [problem["option_A"], problem["option_B"]]:
+        prompt = PREFIX_PROMPT_TEMPLATE.format(qs, option)
+        questions.append(prompt)
+        inputs = processor(images=image, text=prompt, return_tensors="pt").to(device="cuda", dtype=torch.float16)
+        answer_tokens = processor.tokenizer.encode(' ' + option, add_special_tokens=False)
+        num_answer_tokens = len(answer_tokens)
+        input_ids = inputs["input_ids"]
+        # try to find the answer tokens in input ids
+        start_indices = []
+        for i in range(input_ids.size(1) - num_answer_tokens + 1):
+            if torch.equal(input_ids[0, i:i+num_answer_tokens], torch.tensor(answer_tokens).cuda()):
+                start_indices.append(i)
+        
+        if len(start_indices) == 0:
+            raise ValueError("Answer tokens not found in input_ids")
+        answer_start = start_indices[-1]
+        answer_start_from_back = answer_start - input_ids.size(1)
+        with torch.inference_mode():
+            out = model(**inputs
+                )
+            # shift by 1 compared to input
+            logits = out.logits[0, answer_start_from_back-1:answer_start_from_back-1+num_answer_tokens]
+            probs = torch.nn.functional.softmax(logits, dim=-1)
+
+            # Pick the probabilities corresponding to each of the answer tokens
+            probs = torch.gather(probs, 1, torch.tensor(answer_tokens).cuda().unsqueeze(0))
+            prefix_score = torch.prod(probs.pow(1/num_answer_tokens))
+            scores.append(prefix_score.item())
+
+    outputs = "A" if scores[0] > scores[1] else "B"
     return outputs
