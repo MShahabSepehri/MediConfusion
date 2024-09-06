ad373ead8ffe13b5a2fe215c5c934ad49ad7b2bf
z-fabian <zfabian@usc.edu>
2024-09-06 16:45:44 -0700

    add med_flamingo prefix scoring

--- VLMs/med_flamingo.py
+++ VLMs/med_flamingo.py
@@ -58,6 +58,10 @@ def ask_question(model, processor, image_path, question, max_new_tokens, mode, I
     images = [Image.open(image_path) for image_path in tmp]
     pixels = processor.preprocess_images(images)
     pixels = repeat(pixels, 'N c h w -> b N T c h w', b=1, T=1)
+
+    if mode == 'prefix':
+        return do_prefix_forward(model, question, pixels, processor)
+
     question = process_prompt(question, use_option=(mode=='mc'))
     tokenized_data = processor.encode_text(question)
     if mode == 'greedy':
@@ -97,3 +101,43 @@ def do_generation(model, processor, pixels, tokenized_data, max_new_tokens):
     while response[0] == ' ':
         response = response[1: ]
     return response
+
+@torch.no_grad()
+def do_prefix_forward(model, problem, pixels, processor):
+    PREFIX_PROMPT_TEMPLATE =  "You are a helpful medical assistant. You are being provided with images, a question about each image and an answer. Follow the examples and answer the last question. <image>Question: What radiological technique was used to confirm the diagnosis? Answer: Mammography<|endofchunk|><image>Question: What did the CT scan show? Answer: Cerebral edema<|endofchunk|><image>Question: What is the purpose of the asterisk shown in the figure? Answer: To indicate the normal lentoid shape of hypocotyl nuclei.<|endofchunk|><image>Question: {}\nAnswer: {}"
+    scores = []
+    questions = []
+    qs = problem["question"]
+    device = model.lang_encoder.device
+    for option in [problem["option_A"], problem["option_B"]]:
+        prompt = PREFIX_PROMPT_TEMPLATE.format(qs, option)
+        prompt = process_prompt(prompt, use_option=False)
+        tokenized_data = processor.encode_text(prompt)
+        questions.append(prompt)
+        answer_tokens = processor.tokenizer.encode(" " + option, add_special_tokens=False)[1:]
+        num_answer_tokens = len(answer_tokens)
+        input_ids = tokenized_data['input_ids']
+        # try to find the answer tokens in input ids
+        start_indices = []
+        for i in range(input_ids.size(1) - num_answer_tokens + 1):
+            if torch.equal(input_ids[0, i:i+num_answer_tokens], torch.tensor(answer_tokens)):
+                start_indices.append(i)
+        
+        if len(start_indices) == 0:
+            raise ValueError("Answer tokens not found in input_ids")
+        answer_start = start_indices[-1]
+        answer_start_from_back = answer_start - input_ids.size(1)
+        with torch.inference_mode():
+            outputs = model.forward(vision_x=pixels.to(device),
+                            lang_x=tokenized_data["input_ids"].to(device),
+                            attention_mask=tokenized_data["attention_mask"].to(device))
+            # shift by 1 compared to input
+            logits = outputs.logits[0, answer_start_from_back-1:answer_start_from_back-1+num_answer_tokens]
+            probs = torch.nn.functional.softmax(logits, dim=-1)
+
+            # Pick the probabilities corresponding to each of the answer tokens
+            probs = torch.gather(probs, 1, torch.tensor(answer_tokens).cuda().unsqueeze(0))
+            prefix_score = torch.prod(probs.pow(1/num_answer_tokens))
+            scores.append(prefix_score.item())
+    outputs = "A" if scores[0] > scores[1] else "B"
+    return outputs
