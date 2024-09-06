80f140e25699ae8e805873ebdc8b2a44824a7145
shahab <sepehri@usc.edu>
2024-09-06 11:23:37 -0700

    minor change

--- VLMs/llava_med.py
+++ VLMs/llava_med.py
@@ -6,11 +6,8 @@ from llava.conversation import conv_templates
 from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path, tokenizer_image_token, process_images
 from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
-<<<<<<< HEAD
-=======
 from torch.nn import CrossEntropyLoss
 from torch.nn.functional import softmax
->>>>>>> 59d57d6c9480e055c46c3759be40df47f1ef0dfa
 
 def load_model(model_path, model_base):
     disable_torch_init()
