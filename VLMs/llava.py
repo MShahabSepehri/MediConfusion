0c7fa051ade113e4f4ed88d060f09f85864c6fe1
shahab <sepehri@usc.edu>
2024-09-11 14:00:36 -0700

    fixing llava

--- VLMs/llava.py
+++ VLMs/llava.py
@@ -30,7 +30,7 @@ def ask_question(model, processor, question, image, mode, temperature=0.2, top_p
     if mode == 'greedy':
         outputs = do_forward(model, inputs, processor)
     elif mode in ['mc', 'gpt4']:
-        outputs = do_generation(model, inputs, temperature, top_p, num_beams, max_new_tokens)
+        outputs = do_generation(model, inputs, processor, temperature, top_p, num_beams, max_new_tokens)
     return outputs
 
 
@@ -49,8 +49,13 @@ def do_forward(model, inputs, processor):
     return outputs
 
 @torch.no_grad()
-def do_generation(model, inputs, processor, max_new_tokens):
-    output = model.generate(**inputs, max_new_tokens=max_new_tokens)
+def do_generation(model, inputs, processor, temperature, top_p, num_beams, max_new_tokens):
+    output = model.generate(**inputs, 
+                            temperature=temperature, 
+                            do_sample=(temperature > 0),
+                            top_p=top_p, 
+                            num_beams=num_beams, 
+                            max_new_tokens=max_new_tokens)
     return processor.decode(output[0], skip_special_tokens=True)
 
 @torch.no_grad()
